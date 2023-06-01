b57a23d22cff320e9b46dd3976a4d706caba2132
RustyYato <rustyyato@gmail.com>
2023-06-01 12:59:14 -0700

    chose different keys from zero

--- istr/src/hasher.rs
+++ istr/src/hasher.rs
@@ -1,7 +1,13 @@
 use std::hash::{BuildHasher, Hasher};
 
 pub fn hash(value: &[u8]) -> u64 {
-    let mut hasher = ahash::RandomState::with_seeds(0, 0, 0, 0).build_hasher();
+    let mut hasher = ahash::RandomState::with_seeds(
+        3609252661711376574,
+        17522957641342131531,
+        18364184400384450343,
+        5674598519608203581,
+    )
+    .build_hasher();
     hasher.write(value);
     hasher.finish()
 }
