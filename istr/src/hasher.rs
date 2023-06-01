e2eaad01cc29b39dc634af1beeb88a50d472af6c
RustyYato <rustyyato@gmail.com>
2023-06-01 12:30:17 -0700

    switch to a faster hasher

--- istr/src/hasher.rs
+++ istr/src/hasher.rs
@@ -1,7 +1,7 @@
-use std::hash::Hasher;
+use std::hash::{BuildHasher, Hasher};
 
 pub fn hash(value: &[u8]) -> u64 {
-    let mut hasher = rustc_hash::FxHasher::default();
+    let mut hasher = ahash::RandomState::with_seeds(0, 0, 0, 0).build_hasher();
     hasher.write(value);
     hasher.finish()
 }
